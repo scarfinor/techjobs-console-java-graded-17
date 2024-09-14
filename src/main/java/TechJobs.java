756854afe322c3b6cdd9744f121ba2dd6132ecff
scarfinor <scarfinor@gmail.com>
2024-09-13 19:45:20 -0500

    git commit task 1: printJobs refactored to print the data as shown in example. added "No Results"

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
-    }
+            if (someJobs == null) {
+                System.out.println("No Results");
+                return;
+            }
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("\n" + "*****");
+                for (String key : job.keySet()) {
+                    String value = job.get(key);
+                    System.out.println(key + ": " + value);
+                }
+                System.out.println("*****");
+            }
+        }
 }
